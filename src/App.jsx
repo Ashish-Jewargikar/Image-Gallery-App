f953308f8ebef32bf86ef138c8a36f12e3863910
Ashish-Jewargikar <ashish961169@gmail.com>
2023-09-19 12:41:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Gallery } from "./Gallery"
+import "./styles.css";
+
+function App(){
+  return (
+    <>
+      <Gallery/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Gallery.jsx
@@ -0,0 +1,54 @@
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+import 'lightgallery/css/lg-autoplay.css';
+import 'lightgallery/css/lg-fullscreen.css';
+import 'lightgallery/css/lg-share.css';
+import 'lightgallery/css/lg-rotate.css';
+
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgAutoplay from 'lightgallery/plugins/autoplay'
+import lgFullscreen from 'lightgallery/plugins/fullscreen';
+import lgShare from 'lightgallery/plugins/share';
+import lgRotate from 'lightgallery/plugins/rotate';
+
+
+const images=[
+    {src:"img1.jpg",alt:"1"},
+    {src:"img2.jpg",alt:"prompt:srk kid"},
+    {src:"img3.jpg",alt:"3"},
+    {src:"img3.jpg",alt:"3"},
+    {src:"img1.jpg",alt:"2"},
+    {src:"img2.jpg",alt:"3"}
+]
+
+export function Gallery() {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+    return (
+        <div className="App">
+            <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom, lgAutoplay, lgFullscreen, lgRotate, lgShare]}
+            >
+
+            {images.map((
+                images,index)=>{
+                    return (
+                        <a href={images.src} key={index}>
+                        <img alt={images.alt} src={images.src}/>
+                        </a>
+                    )
+                })}
+            </LightGallery>
+        </div>
+    );
+}
